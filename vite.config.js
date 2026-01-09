b818aabeb5078bc7216c133b119e44a1b66711dd
Anzalas <anzalas.muhammad@gmail.com>
2026-01-09 15:05:17 +0700

    9 jan .6

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,33 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["icons/icon-192.png", "icons/icon-512.png"],
+      manifest: {
+        name: "Java Cell",
+        short_name: "Java Cell",
+        start_url: "/login?source=pwa",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#1e40af",
+        icons: [
+          {
+            src: "/vite.svg",
+            sizes: "192x192",
+            type: "image/svg",
+          },
+          {
+            src: "/vite.svg",
+            sizes: "512x512",
+            type: "image/svg",
+          },
+        ],
+      },
+    }),
+  ],
 });
